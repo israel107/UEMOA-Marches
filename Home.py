12fc7e8d7ff0b893370472616405267a74ab2d3c
Christ107 <68905411+israel107@users.noreply.github.com>
2025-04-15 14:51:01 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -36,7 +36,7 @@ all_period = ["Année","Semestre","Trimestre","Mois"]
 df_index = pd.read_excel("./All_Indices_2024.xlsx", sheet_name="Cours") 
 df_val = pd.read_excel("./idx_val_sept2024_2.xlsx" , sheet_name=None)
 df_vol = pd.read_excel("./idx_volume_sept2024.xlsx", sheet_name=None)
-#df_oblig = pd.read_excel("./Val_Oblig_sept_2024.xlsx", sheet_name=None)
+df_oblig = pd.read_excel("./Val_Oblig_sept_2024.xlsx", sheet_name=None)
 df_oblig.set_index('seance', inplace=True)
 
 df_index_2 = df_index
