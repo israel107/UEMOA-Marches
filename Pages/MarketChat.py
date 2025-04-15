a0e07a8b84c91f11eafa799acd6e6b276b00c22f
Christ107 <68905411+israel107@users.noreply.github.com>
2025-04-15 13:55:48 +0000

    initial commit

---
+++ Pages/MarketChat.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+import plost
+import numpy as np
+import ollama
+
+
+with open("style2.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html= True)
+
+#st.set_page_config(page_title="Market Chat", page_icon="", layout="wide")  
+
+# st.text_input("_Ecrivez votre question_", key="styledinput")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+st.subheader("Que voudriez-vous savoir sur le marché de l'UEMOA ?")
+if prompt := st.chat_input("Comment pourrais-je vous aider ?"):
+    #user
+    st.session_state.messages.append({"role":"user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.write(prompt)
+    
+    #model
+    with st.chat_message("assistant"):
+        response = ollama.chat(model='gemma2', messages=[
+            {'role': m['role'], "content": m["content"]}
+            for m in st.session_state.messages
+        ], stream=True)
+
+        response_content = " "
+        def catch_response(response):
+            global response_content 
+            for chunk in response:
+                response_content +=chunk['message']['content']
+                yield chunk['message']['content']
+
+        stream = catch_response(response)
+        st.write_stream(stream)
+
+        st.session_state.messages.append({"role": "assistant", "content": response_content})
+
+# with st.expander("⏱ Filter Tabulation"):
+#  #plot tabulation
+#  tab=pd.crosstab([df_selection["gender"],df_selection["comment"]],df_selection["stream"],margins=True)
+#  st.dataframe(tab,use_container_width=True)
+#  #downloading link
+#  csv1=convert_df(tab)
+#  st.download_button("Press to Download",csv1,"yourfile.csv",key='download-csv')
