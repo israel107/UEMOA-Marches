03d32bbd0ed405f9e2b8068fcd5646e37934c51c
Christ107 <68905411+israel107@users.noreply.github.com>
2025-04-26 12:54:53 +0000

    Create m_Home.py

---
+++ Pages/m_Home.py
@@ -0,0 +1,9 @@
+import random
+#import duckdb
+import pandas as pd
+import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import streamlit as st
+import plost
+import numpy as np
