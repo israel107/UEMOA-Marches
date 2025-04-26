f74d610f6c97c83cfd498cb4406be9b7b25857f3
Christ107 <68905411+israel107@users.noreply.github.com>
2025-04-26 13:35:17 +0000

    Update m_Home.py

--- Pages/m_Home.py
+++ Pages/m_Home.py
@@ -72,6 +72,7 @@ with st.sidebar:
 #######################################
 # DATA INDICATEUR
 #######################################
+st.html("<h1 style=' border: 2px solid #fff; padding: 20px; text-align: center; background-color: #002b36; font-family: Arial, sans-serif; width: 100%; margin: 20px auto; border-radius: 10px; box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.1);'><marquee>💎Diamonds App - Prediction💎</marquee></h1>")
 with st.expander("STATISTIQUES"):
     st.markdown('### Infos essentielles')
     m_cap_obl = [7100, 12000, 9500, 10500, 14100, 11600]
